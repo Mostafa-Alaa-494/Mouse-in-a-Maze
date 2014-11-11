f2eb7ae688ba4497bd909d59b070029cc7c2ee65
Salma Farag Galal <SalmaFG@users.noreply.github.com>
2014-11-11 19:19:21 +0100

    Update Vertical.java

--- Mouse/src/Vertical.java
+++ Mouse/src/Vertical.java
@@ -1,10 +1,11 @@
 public class Vertical extends Tile{
 
-	public Vertical(int id, int size, int[]x, int []y) {
-		super.setId(id);
+	public Vertical(int size, int[]x, int []y) {
+		super.setId(staticID);
 		super.setType('v');
 		super.setSize(size);
 		super.setX(x);
 		super.setY(y);
+		staticID++;
 	}
 }
