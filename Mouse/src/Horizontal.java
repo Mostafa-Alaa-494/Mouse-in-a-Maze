0225ca00fc63b5224545e50d0e619d1a5e86e523
Salma Farag Galal <SalmaFG@users.noreply.github.com>
2014-11-11 19:18:53 +0100

    Update Horizontal.java

--- Mouse/src/Horizontal.java
+++ Mouse/src/Horizontal.java
@@ -1,10 +1,11 @@
 public class Horizontal extends Tile {
 	
-	public Horizontal(int id, int []x, int []y) {
-		super.setId(id);
+	public Horizontal(int []x, int []y) {
+		super.setId(staticID);
 		super.setType('h');
 		super.setSize(2);
 		super.setX(x);
 		super.setY(y);
+		staticID++;
 	}
 }
