49adbd88a478e48e77bc17d7ef10e709e8849735
Salma Farag Galal <SalmaFG@users.noreply.github.com>
2014-11-12 20:51:47 +0100

    Create Search.java

---
+++ Mouse/src/Search.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public abstract class Search {
+	String name;
+	Tree tree;
+	LinkedList<Tree.Node> q;
+	
+	public Tree.Node GeneralSearch(Problem p){
+		return null;
+	}
+}
